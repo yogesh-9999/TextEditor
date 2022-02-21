e824e572d709bf7fbc2035b0898fa2b217a1a733
Yogesh Chand Upadhyay <20bec126@iiitdmj.ac.in>
2022-02-21 14:59:29 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import About from './component/About';
+import Navbar from './component/Navbar';
+import TextForm from './component/Textform';
+import React, { useState } from 'react';
+ 
 function App() {
+
+  const[statee1,setstatee1]= useState('enable');
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      setstatee1('disable');
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      setstatee1('Enable');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   
+    <Navbar title="TextEditor" mode={mode} toggleMode={toggleMode} statee={statee1} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+
+
+   
     </div>
+
+    </> 
   );
 }
 
---
+++ src/component/Textform.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+export default function TextForm(props) {
+    
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+   const handleLoClick=()=>{
+       let txt=text.toLowerCase();
+       setText(txt);
+   }
+
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value)
+    }
+    const ramuu=()=>{
+        document.getElementById('good').innerHTML+="change"
+    }
+    const SOMEF=()=>{
+        // ramuu();
+        handleLoClick();
+    }
+    const handleCopy = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+    const [text, setText] = useState('Enter text here2'); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{backgroundcolour: props.mode==='dark'? 'Grey': 'white', color:props.mode=== 'dark'?'white': 'black'}}> 
+            <h1>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" id='good' onClick={SOMEF}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove extra spaces</button>
+        </div>
+
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Enter something in the above textbox to preview it here"}</p>
+            
+        </div>
+        </>
+    
+    )
+}
